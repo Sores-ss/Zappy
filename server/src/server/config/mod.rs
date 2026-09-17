6c3b7f21ac76c1216e22aa687b4b66fd2c4d33a0
Eros <eros.delianne-le-boucher@epitech.eu>
2026-09-17 13:58:01 +0200

    [EDLB][GUI] feat: egg spawn/hatch/death effects and suc/sbp handling

--- server/src/server/config/mod.rs
+++
@@ -1,8 +0,0 @@
-//
-// EPITECH PROJECT, 2026
-// Zappy
-// File description:
-// mod
-//
-
-pub mod config;
--- server/src/server/server.rs
+++ server/src/server/server.rs
@@ -5,27 +5,56 @@
 // Server
 //
 
-use super::config::config::Config;
-use super::map::map::Map;
+use super::game::world::World;
+use super::config::Config;
+use super::net::reactor::Reactor;
+use std::io::Result;
+use std::net::TcpListener;
 
 pub struct Server {
-    pub _config: Config,
-    pub _map: Map,
+    config: Config,
+    listener: Option<TcpListener>,
 }
 
 impl Server {
-    pub fn new() -> Self {
-        let mut serv: Server = Server {
-            _config: Config::new(4242, 10, vec!["GRAPHICAL".to_string()], 100, (10, 10)),
-            _map: Map::new(10, 10),
-        };
-        serv._config.parse_config();
-        serv._map.define_size(serv._config._world_size);
-        serv
+    pub fn new(config: Config) -> Self {
+        Server {
+            config,
+            listener: None,
+        }
     }
 
-    pub fn start(&self) {
-        println!("Server is starting on port {}", self._config._port);
-        // Additional logic to start the server can be added here
+    pub fn start(&mut self) -> Result<()> {
+        println!("Server configuration:");
+        println!("Port: {}", self.config.port);
+        println!("Max Clients: {}", self.config.clients);
+        println!("Teams Names: {:?}", self.config.names);
+        println!("Time Unit: {}", self.config.frequency);
+        println!("World Size: {}x{}", self.config.x, self.config.y);
+
+        let listener = TcpListener::bind(format!("127.0.0.1:{}", self.config.port))?;
+        listener.set_nonblocking(true)?;
+        println!("Server started on port {}", listener.local_addr()?.port());
+        self.listener = Some(listener);
+        Ok(())
+    }
+
+    pub fn run(&mut self) -> Result<()> {
+        let listener = match self.listener.take() {
+            Some(listener) => listener,
+            None => {
+                eprintln!("Server is not running. Please start the server first.");
+                return Ok(());
+            }
+        };
+
+        let map = World::new(
+            self.config.x,
+            self.config.y,
+            &self.config.names,
+            self.config.clients,
+        );
+        let mut reactor = Reactor::new(listener, self.config.frequency, map)?;
+        reactor.run()
     }
 }
