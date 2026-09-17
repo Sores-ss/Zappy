bed69700696732a8e22ca60ec794ff669cb1ad50
Eros <eros.delianne-le-boucher@epitech.eu>
2026-09-17 13:55:02 +0200

    [CNE][server/src/config.rs] remove clap dependencie and add vanialla rust parsing

--- server/src/server/config.rs
+++ server/src/server/config.rs
@@ -4,33 +4,96 @@
 // File description:
 // Config
 //
-use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[command(version, about = "Zappy Server Configuration")]
+#[derive(Debug)]
 pub struct Config {
     // Define Port Number
-    #[arg(short, long, default_value = "4242", help = "Port number to listen on")]
     pub port: u16,
-    // Define Max Clients 
-    #[arg(short, long, default_value = "10", help = "Number of authorized clients per team")]
+    // Define Max Clients
     pub clients: usize,
     // Define Teams Names
-    #[arg(required = true, short, long, num_args = 1.., default_value = "GRAPHICAL", help = "Names of the teams (one or more)")]
     pub names: Vec<String>,
-    // Define Time Unit 
-    #[arg(short, long, default_value = "1000", help = "Server frequency (time unit in ms)")]
+    // Define Time Unit
     pub frequency: u32,
-    // Define World width 
-    #[arg(short, long, default_value = "10", help = "World width in tiles")]
+    // Define World width
     pub x: usize,
-    // Define World height 
-    #[arg(short, long, default_value = "10", help = "World height in tiles")]
+    // Define World height
     pub y: usize,
 }
 
 impl Config {
+    fn default() -> Self {
+        Config {
+            port: 4242,
+            clients: 10,
+            names: vec!["GRAPHICAL".to_string()],
+            frequency: 1000,
+            x: 10,
+            y: 10,
+        }
+    }
+
+    fn print_help() {
+        println!("Zappy Server Configuration");
+        println!();
+        println!("USAGE:");
+        println!("    zappy_server [OPTIONS]");
+        println!();
+        println!("OPTIONS:");
+        println!("    -p <port>       Port number to listen on [default: 4242]");
+        println!("    -c <clients>    Number of authorized clients per team [default: 10]");
+        println!("    -n <names>...   Names of the teams (one or more) [default: GRAPHICAL]");
+        println!("    -f <frequency>  Server frequency (time unit in ms) [default: 1000]");
+        println!("    -x <x>          World width in tiles [default: 10]");
+        println!("    -y <y>          World height in tiles [default: 10]");
+        println!("    -h, --help      Print help information");
+    }
+
     pub fn new() -> Self {
-        Config::parse()
+        let mut config = Config::default();
+        let args: Vec<String> = std::env::args().skip(1).collect();
+        let mut i = 0;
+
+        while i < args.len() {
+            match args[i].as_str() {
+                "-h" => {
+                    Config::print_help();
+                    std::process::exit(0);
+                }
+                "-p" => {
+                    i += 1;
+                    config.port = args[i].parse().expect("invalid value for port");
+                }
+                "-c" => {
+                    i += 1;
+                    config.clients = args[i].parse().expect("invalid value for clients");
+                }
+                "-n" => {
+                    let mut names = Vec::new();
+                    while i + 1 < args.len() && !args[i + 1].starts_with('-') {
+                        i += 1;
+                        names.push(args[i].clone());
+                    }
+                    if !names.is_empty() {
+                        config.names = names;
+                    }
+                }
+                "-f" => {
+                    i += 1;
+                    config.frequency = args[i].parse().expect("invalid value for frequency");
+                }
+                "-x" => {
+                    i += 1;
+                    config.x = args[i].parse().expect("invalid value for x");
+                }
+                "-y" => {
+                    i += 1;
+                    config.y = args[i].parse().expect("invalid value for y");
+                }
+                other => panic!("unexpected argument: {other}"),
+            }
+            i += 1;
+        }
+        config
     }
 }
